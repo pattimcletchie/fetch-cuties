78655599ab5cebfe6c080c11701701d421ca42d6
Patti McLetchie <pattimcletchie@gmail.com>
2021-03-17 13:41:55 -0500

    updating prop name in kitten

--- my-app/src/components/Kitten.jsx
+++ my-app/src/components/Kitten.jsx
@@ -4,7 +4,7 @@ import Photo from './Photo';
 
 // create Kitten component
 const Kitten = () => {
-  const [picture, setPicture] = useState(0);
+  const [pictureUrl, setPictureUrl] = useState(0);
 
   // get kitten picture
   function getImage() {
@@ -13,30 +13,35 @@ const Kitten = () => {
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
-    }).then((response) => {
-      console.log('response', response);
-      if (!response.ok) {
-        throw Error('Error fetching your cutie');
+    }).then(
+      (response) => {
+        console.log('response', response);
+        if (!response.ok) {
+          throw Error('Error fetching your cutie');
+        }
+        return (response) =>
+          response
+            .text()
+            .then((text) => console.log(text))
+            .json()
+            .then((allData) => {
+              setPictureUrl({ photos: allData });
+            })
+            .catch((err) => {
+              throw Error(err.message);
+            });
+      },
+      (error) => {
+        console.log(error);
       }
-      return (response) =>
-        response
-          .text()
-          .then((text) => console.log(text))
-          .json()
-          .then((allData) => {
-            setPicture({ photos: allData });
-          })
-          .catch((err) => {
-            throw Error(err.message);
-          });
-    });
+    );
   }
 
   return (
     <div>
       <h3>Awesome Cat</h3>
       <div className="displayFlex">
-        <Photo photos={picture.url} />
+        <Photo url={pictureUrl} />
         <button onClick={getImage}>Get a Cutie</button>
       </div>
     </div>
